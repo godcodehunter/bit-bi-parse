6507875ee941d9bd19987cd4f2146207cd645836
godcodehunter <mamhigtt@gmail.com>
2024-10-15 20:13:56 +0700

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,7 @@ use std::ops::{Index, IndexMut};
 
 /// Maximum value that N bits can store
 pub fn bits_to_max_hold(bit_size: u32) -> u32 {
-    // For example, `bit_size` is 6
-    // Then max number is 111111
-    // The translation of the value occurs 
-    // according to the formula 
-    // 2^5 + 2^4 + 2^3 + 2^2 + 2^1 + 2^0
-    //
-    // Then, in general, from 0 to N:
-    let mut acc = 0;
-    for n in 0..bit_size {
-        acc += 2u32.pow(n);
-    }
-    acc
+    (1 << bit_size) - 1
 }
 
 #[test]
